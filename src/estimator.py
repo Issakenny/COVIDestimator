af5abfe07ae26826e79ac9be0fa4730e679437d7
Issakenny <issakehinde@yahoo.com>
2020-04-14 18:35:31 +0000

    'SevereCasesByRequestedTime calculated' zx

--- src/estimator.py
+++ src/estimator.py
@@ -1,9 +1,10 @@
 def estimator(data):
 
   return data
+
 data = {'name': 'Africa', 'avg_age': 19.7, 'avg_daily_income': 5, 'avg_daily_income_population': 0.71,
                 'period_type': 'daily', 'time_to_elapse': 58, 'reported_cases': 674, 'population': 66622705,
-                'total_hospital_beds': 1380614}
+                'total_hospital_beds': 1380614 }
 
 
 impact_currentlyInfected = ((data.get('reported_cases')) * 10)
